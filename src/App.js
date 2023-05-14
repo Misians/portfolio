230ec6557eedd946c8183068650cdb5c30ab55c4
Misians <artemisiakimberlly@alu.uern.br>
2023-05-14 16:49:22 -0300

    TESTE

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Navbar from "./Components/navbar/Navbar";
 import './App.css'
 import Intro from "./Components/navbar/Intro/Intro";
 import Services from "./Components/Services/Services";
+import Contact from "./Components/Contact/Contact";
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Intro/>
       <Services />
+      <Contact />
     </div>
   );
 }
